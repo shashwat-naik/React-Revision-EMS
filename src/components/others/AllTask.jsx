0e9054cc7c1e63887531a2ed9d033c8086faa1d2
Shashwat Naik <naikshashwat3@gmail.com>
2025-09-28 17:43:28 +0530

    Admin Dashboard

---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const AllTask = () => {
+  return (
+    <div className="bg-[#1c1c1c] p-5 rounded mt-5 h-52 overflow-auto">
+      <div className="bg-blue-500 mb-2 py-2 px-4 flex justify-between rounded">
+        <h2>Shashwat</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-purple-500 mb-2 py-2 px-4 flex justify-between rounded">
+        <h2>Shashwat</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-green-500 mb-2 py-2 px-4 flex justify-between rounded">
+        <h2>Shashwat</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-amber-500 mb-2 py-2 px-4 flex justify-between rounded">
+        <h2>Shashwat</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className="bg-red-500 mb-2 py-2 px-4 flex justify-between rounded">
+        <h2>Shashwat</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  );
+};
+
+export default AllTask;
