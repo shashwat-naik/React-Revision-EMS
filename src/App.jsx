fc87f0c10526d2069d81e615eaf1e05248321982
Shashwat Naik <naikshashwat3@gmail.com>
2025-10-04 21:21:44 +0530

    Last left on 2:07:27

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
-import { setLocalStorage } from "./utils/localStorage";
+import { AuthContext } from "./context/AuthProvider";
 
 const App = () => {
-  /* useEffect(() => {
-    setLocalStorage();
-  }); */
-
   const [user, setUser] = useState(null);
+  const authData = useContext(AuthContext);
+
+  useEffect(() => {
+    if (authData) {
+      const loggedInUser = localStorage.getItem("LoggedInUser");
+      if(loggedInUser){
+        setUser(loggedInUser.role)
+      }
+    }
+  }, [authData]);
 
   const handleLogin = (email, password) => {
     if (email == "admin@me.com" && password == "123") {
-      setUser("admin");
-    } else if (email == "user@me.com" && password == "123") {
-      setUser("employee");
+      setUser({role:'admin'});
+      localStorage.setItem('loggedInUser', JSON.stringify({role:'admin'}))
+    } else if (authData ){
+      const employee=authData.employees.find((e) => email == e.email && e.password == password)
+      if(employee){
+        setUser({role:'employee'});
+      }
+      localStorage.setItem('loggedInUser', JSON.stringify({role:'employee'}))
     } else {
       alert("Invalid credentials");
     }
   };
+
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ""}
--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -201,4 +201,6 @@ export const setLocalStorage = () => {
 export const getLocalStorage = () => {
   const employees = JSON.parse(localStorage.getItem("employees"));
   const admin = JSON.parse(localStorage.getItem("admin"));
+
+  return {employees, admin}
 };
